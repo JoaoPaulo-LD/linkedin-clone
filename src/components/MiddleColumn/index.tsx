028994581d33629be2965b90a68483486bb485ab
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-08 14:25:41 -0300

    Shimmer effect on profile panel applied

--- src/components/MiddleColumn/index.tsx
+++ src/components/MiddleColumn/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import LoadingFeedPost from '../Shimmer/LoadingFeedPost'
+
 import FeedShare from './FeedShare'
 import FeedPost from './FeedPost'
 
@@ -8,11 +10,20 @@ import { Container } from './styles'
 const MiddleColumn: React.FC<LoadingProps> = ({ isLoading }) => {
   return (
     <Container className="middle-column">
-      <FeedShare />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      {isLoading ? (
+        <>
+          {/* <LoadingFeedShare /> */}
+          <LoadingFeedPost />
+        </>
+      ) : (
+        <>
+          <FeedShare />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+          <FeedPost />
+        </>
+      )}
     </Container>
   )
 }
---
+++ src/components/Shimmer/LoadingFeedPosts/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const LoadingFeedPosts: React.FC = () => {
+  return (
+    <Container>
+      <h1>LoadingFeedPosts</h1>
+    </Container>
+  )
+}
+
+export default LoadingFeedPosts
