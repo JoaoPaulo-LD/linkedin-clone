634f3ae127434eea88720f6b7e4ccaa84660ec70
JoaoPaulo-LD <joaopauloalencarrodrigues@gmail.com>
2020-10-08 13:05:50 -0300

    DesktopHeader component finished

--- src/components/DesktopHeader/index.tsx
+++ src/components/DesktopHeader/index.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  Wrapper,
+  LinkedInIcon,
+  SearchInput,
+  HomeIcon,
+  NotificationsIcon,
+  ProfileCircle,
+  CaretDownIcon
+} from './styles';
 
 const DesktopHeader: React.FC = () => {
   return (
     <Container>
-      <h1>DesktopHeader</h1>
+      <Wrapper>
+        <div className="left">
+          <LinkedInIcon />
+          <SearchInput placeholder="Pesquisar" />
+        </div>
+
+        <div className="right">
+          <nav>
+            <button className="active">
+              <HomeIcon />
+              <span>Início</span>
+            </button>
+            <button>
+              <NotificationsIcon />
+              <span>Notificações</span>
+            </button>
+
+            <button>
+              <ProfileCircle src="https://github.com/joaopaulo-ld.png" />
+              <span>
+                Eu <CaretDownIcon />
+              </span>
+            </button>
+          </nav>
+        </div>
+      </Wrapper>
     </Container>
   );
 };
